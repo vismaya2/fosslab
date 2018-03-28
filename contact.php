ad606208488867cd2a59ee2b6e6df15d1e216d1e
vismaya2 <37132901+vismaya2@users.noreply.github.com>
2018-03-28 10:44:06 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<body style="background-color:#fef9e7;">
+<img src="clg3.jpg" alt=" GEC PALAKKAD"style="width:1200px;height:400px;" align="center;"><br>
+<h1 style="color:#212f3d" align="center">GOVERNMENT ENGINEERING COLLEGE SREEKRISHNAPURAM</h1>
+
+<h2 style="color:#0b5345" align="center"><b>GEC PKD STUDENTS ASSOCIATION</b></h2>
+
+<p> <b>Contact us: +91 466 2260350 <br> Email us: principal@gecskp.ac.in <br></p>
+<a href="https://www.google.co.in/maps/place/Govt.+Engineering+College+Palakkad/@10.903552,76.4325503,17z/data=!3m1!4b1!4m5!3m4!1s0x3ba7d635151d9b73:0xecf06761ecf4f56c!8m2!3d10.903552!4d76.434739">Route map</a>
+
+
---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<body style="background-color:#d7bde2;">
+<img src="clg3.jpg" alt=" GEC PALAKKAD"style="width:1200px;height:400px;" align="center;"><br>
+
+  <a href="home.php">HOME</a>
+  <a href="department.php">DEPARTMENT</a>
+  <a href="contact.php">CONTACT US</a>
+  <a href="login.php">LOGIN</a>
+
+<h1 style="color:#212f3d" align="center">GOVERNMENT ENGINEERING COLLEGE SREEKRISHNAPURAM</h1>
+
+<h2 style="color:#0b5345" align="center"><b>GEC PKD STUDENTS ASSOCIATION</b></h2>
+
+<p> Government Engineering College Sreekrishnapuram is a premier educational institute established in 1999. It is situated in Palakkad district of Kerala. The college is fully owned by Govt.of Kerala and is under the Directorate of Technical Education, Kerala. GEC Sreekrishnapuram is approved by the AICTE and is affiliated to the University of Calicut and APJ Abdul kalam Technological University (from 2015 onwards).<br >
+
+The college offers 4-year full-time B.Tech courses in the following branches:<br >1) Computer Science and Engineering <br>2) Electronics and Communication Engineering <br>3)Information Technology <br>4)Mechanical Engineering <br>5)Electrical and Electronics Engineering. <br>It also conducts an M.Tech course in Computational Linguistics in the Dept. of Computer Science and Engineering from 2011 onwards.Institution offers PHD under KTU since 2016 onwards </p>
+
+</body>
+</html>
+
